aacade24ef41e60d46bf3f21477e9f333d140129
bestb4 <67499665+bestb4@users.noreply.github.com>
2021-04-13 22:14:14 +0900

    Calendar update

---
+++ src/calendar/Scanner.java
@@ -0,0 +1,16 @@
+package calendar;
+ 
+import java.util.Scanner;
+ 
+public class Scanner {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+        System.out.println(name+"님의 나이는 "+age+"살 입니다.");
+        
+    }
+ 
+}
+
